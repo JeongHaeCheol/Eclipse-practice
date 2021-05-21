0a73c399957a33f584181334ff7315aaa6b24613
JeongHaeCheo <eiop77@naver.com>
2021-05-21 17:32:47 +0900

    JHC practice eclipse work

---
+++ src/Driver.java
@@ -0,0 +1,16 @@
+
+enum HerName {
+	자스민, 박하, 율무, 어성초
+}
+
+
+
+public class Driver {
+
+
+	public static void main(String[] args) {
+	
+		HerName herb = HerName.박하;
+		System.out.println(herb);
+	}
+}
